96d5f5acb6c9261acdfe15c677005a8f6f2f6348
AldoReghan <aldoreghan2217@gmail.com>
2021-11-28 19:39:05 +0700

    Menambahkan Admin Home dan Laporan Activity

--- app/src/main/java/com/sib/picktrash/admin/AdminHomeActivity.kt
+++ app/src/main/java/com/sib/picktrash/admin/AdminHomeActivity.kt
@@ -8,5 +8,6 @@ class AdminHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
+        getActionBar()?.hide()
     }
 }
