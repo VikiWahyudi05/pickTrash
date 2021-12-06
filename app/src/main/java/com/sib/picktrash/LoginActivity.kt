bd22bbeddcbbb48603e142df1d0b7d8dc622bb69
VikiWahyudi05 <vickywahyudi05@gmail.com>
2021-12-06 09:19:01 +0700

    Menambahkan Firebase Login dan Register

--- app/src/main/java/com/sib/picktrash/LoginActivity.kt
+++ app/src/main/java/com/sib/picktrash/LoginActivity.kt
@@ -1,15 +1,103 @@
 package com.sib.picktrash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sib.picktrash.admin.AdminHomeActivity
 import com.sib.picktrash.databinding.ActivityLoginBinding
+import com.sib.picktrash.user.UserHomeActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var store: FirebaseFirestore
+    private var valid = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        actionBar?.hide();
+
+
+        auth = FirebaseAuth.getInstance()
+        store = FirebaseFirestore.getInstance()
+
+        binding.apply {
+            btnLogin.setOnClickListener {
+                checkField(etEmail)
+                checkField(etPassword)
+
+                if (valid) {
+                    auth.signInWithEmailAndPassword(
+                        etEmail.getText().toString(),
+                        etPassword.getText().toString()
+                    )
+                        .addOnSuccessListener { authResult ->
+                            Toast.makeText(this@LoginActivity, "Sukses Login", Toast.LENGTH_SHORT)
+                                .show()
+                            checkUserAccessLevel(authResult.user!!.uid)
+                        }.addOnFailureListener {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Password salah",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                }
+            }
+
+            btnRegister.setOnClickListener(View.OnClickListener {
+                startActivity(
+                    Intent(
+                        applicationContext,
+                        RegisterActivity::class.java
+                    )
+                )
+            })
+        }
+    }
+
+    private fun checkUserAccessLevel(uid: String) {
+        val df = store.collection("Users").document(uid)
+
+        df.get().addOnSuccessListener { documentSnapshot ->
+            Log.d("TAG", "onSuccess: " + documentSnapshot.data)
+
+            if (documentSnapshot.getString("isUser") != null) {
+                Toast.makeText(this@LoginActivity, "Selemat datang User", Toast.LENGTH_LONG).show()
+                startActivity(Intent(applicationContext, UserHomeActivity::class.java))
+                finish()
+            }
+            if (documentSnapshot.getString("isAdmin") != null) {
+                Toast.makeText(this@LoginActivity, "Selamat datang Admin", Toast.LENGTH_LONG).show()
+                startActivity(Intent(applicationContext, AdminHomeActivity::class.java))
+                finish()
+            }
+        }
+
+    }
+
+    fun checkField(textField: EditText?): Boolean {
+        if (textField!!.text.toString().isEmpty()) {
+            textField.error = "Error"
+            valid = false
+        } else {
+            valid = true
+        }
+        return valid
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            checkUserAccessLevel(FirebaseAuth.getInstance().currentUser!!.uid)
+        }
     }
 }
