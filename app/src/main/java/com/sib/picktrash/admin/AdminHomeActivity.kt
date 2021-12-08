2fc9ce281edfde4e83c0d80e619c80945df14ee2
AldoReghan <aldoreghan2217@gmail.com>
2021-12-08 07:14:04 +0700

    Menambahkan fungsi lokasi

--- app/src/main/java/com/sib/picktrash/admin/AdminHomeActivity.kt
+++ app/src/main/java/com/sib/picktrash/admin/AdminHomeActivity.kt
@@ -3,19 +3,25 @@ package com.sib.picktrash.admin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.sib.picktrash.LoginActivity
 import com.sib.picktrash.RegisterActivity
 import com.sib.picktrash.databinding.ActivityAdminHomeBinding
 
 class AdminHomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAdminHomeBinding
+    private lateinit var store: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        store = FirebaseFirestore.getInstance()
 
         binding.logout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -32,5 +38,17 @@ class AdminHomeActivity : AppCompatActivity() {
                 )
             )
         })
+
+        getAllData()
+    }
+
+    fun getAllData(){
+        store.collection("Users").get().addOnSuccessListener {
+//            for (result in it.documents){
+//                Log.i("firedata","${result.data?.values}")
+//            }
+            Log.i("firedata","${it.documents.get(0).data?.get("UserName")}")
+            binding.tvNamaAdmin.text = "${it.documents.get(0).data?.get("UserName")}"
+        }
     }
 }
--- app/src/main/java/com/sib/picktrash/user/UserHomeActivity.kt
+++ app/src/main/java/com/sib/picktrash/user/UserHomeActivity.kt
@@ -1,20 +1,45 @@
 package com.sib.picktrash.user
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.sib.picktrash.LoginActivity
 import com.sib.picktrash.RegisterActivity
 import com.sib.picktrash.databinding.ActivityUserHomeBinding
 
 class UserHomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityUserHomeBinding
+    private lateinit var store: FirebaseFirestore
+
+    private val LOCATION_PERMISSION_REQ_CODE = 1000;
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        store = FirebaseFirestore.getInstance()
 
         binding.logout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -30,6 +55,56 @@ class UserHomeActivity : AppCompatActivity() {
                 )
             )
         })
+
+        getAllData()
+        getCurrentLocation()
     }
 
+    fun getAllData(){
+        store.collection("Users").get().addOnSuccessListener {
+            for (result in it.documents){
+                Log.i("firedata","${result.data?.values}")
+            }
+//            Log.i("firedata","${it.documents.get(0).data?.get("UserName")}")
+        }
+    }
+
+    private fun getCurrentLocation() {
+        // checking location permission
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request permission
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQ_CODE);
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                // getting the last known or current location
+                latitude = location.latitude
+                longitude = location.longitude
+                Log.i("location","${location.latitude}+' '+${location.longitude}")
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed on getting current location",
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQ_CODE -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                } else {
+                    // permission denied
+                    Toast.makeText(this, "You need to grant permission to access location",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
