184af1582090e1aa14ad4e9af6f210af1a604cac
VikiWahyudi05 <vickywahyudi05@gmail.com>
2021-12-14 12:11:42 +0700

    menambahkan laporan adapter admin

---
+++ app/src/main/java/com/sib/picktrash/admin/LaporanAdapter.kt
@@ -0,0 +1,29 @@
+package com.sib.picktrash.admin
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sib.picktrash.databinding.ItemLaporanBinding
+
+class LaporanAdapter (private val laporan:List<LaporanModel>) : RecyclerView.Adapter<LaporanAdapter.LaporanViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaporanViewHolder {
+        val binding = ItemLaporanBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return LaporanViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LaporanViewHolder, position: Int) {
+        holder.bind(laporan[position])
+    }
+
+    override fun getItemCount(): Int = laporan.size
+
+    inner class LaporanViewHolder(private val binding: ItemLaporanBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(laporan: LaporanModel) {
+            with(binding) {
+                tvNama.text = laporan.name
+                tvDeskripsi.text = laporan.description
+            }
+        }
+    }
+}
